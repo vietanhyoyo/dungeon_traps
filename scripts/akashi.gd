21a945a26686bac821328a5b1b521d19b47789f1
Tran Thanh Tien <tran.thanh.tien@vhec.vn>
2026-04-07 15:35:07 +0700

    update attack

--- scripts/akashi.gd
+++ scripts/akashi.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
-
 const SPEED = 180.0
 const JUMP_VELOCITY = -320.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 var is_attacking = false
+var use_attack_1 = true
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -16,7 +16,13 @@ func _physics_process(delta: float) -> void:
 	# Attack input
 	if Input.is_action_just_pressed("attack") and not is_attacking:
 		is_attacking = true
-		animated_sprite.play("attack")
+		
+		if use_attack_1:
+			animated_sprite.play("attack")
+		else:
+			animated_sprite.play("attack2")
+			
+		use_attack_1 = !use_attack_1
 		
 	# Trong lúc attack thì không override animation
 	if is_attacking:
@@ -55,6 +61,6 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 func _on_animated_sprite_2d_animation_finished():
-	if animated_sprite.animation == "attack":
+	if animated_sprite.animation == "attack" or animated_sprite.animation == "attack2":
 		is_attacking = false
 		animated_sprite.play("idle")
