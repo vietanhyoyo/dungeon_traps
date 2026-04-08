f52870f0c8a26e3237b1e2d5c01cf796b0939c00
Tran Thanh Tien <tran.thanh.tien@vhec.vn>
2026-04-08 10:19:06 +0700

    update screen

--- scripts/akashi.gd
+++ scripts/akashi.gd
@@ -16,13 +16,18 @@ func _physics_process(delta: float) -> void:
 	# Attack input
 	if Input.is_action_just_pressed("attack") and not is_attacking:
 		is_attacking = true
-		
-		if use_attack_1:
-			animated_sprite.play("attack")
-		else:
+
+		# Nếu đang trên không thì luôn dùng attack2
+		if not is_on_floor():
 			animated_sprite.play("attack2")
+		else:
+			# Ở dưới đất thì xen kẽ
+			if use_attack_1:
+				animated_sprite.play("attack")
+			else:
+				animated_sprite.play("attack2")
 			
-		use_attack_1 = !use_attack_1
+			use_attack_1 = !use_attack_1
 		
 	# Trong lúc attack thì không override animation
 	if is_attacking:
@@ -30,11 +35,11 @@ func _physics_process(delta: float) -> void:
 		move_and_slide()
 		return
 
-	# Handle jump.
+	# Handle jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction: -1, 0, 1.
+	# Get the input direction: -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
 	
 	# Flip the Sprite
@@ -43,15 +48,6 @@ func _physics_process(delta: float) -> void:
 	elif direction < 0:
 		animated_sprite.flip_h = true
 		
-	# Play animations
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("idle")
-		else: 
-			animated_sprite.play("run") 
-	else: 
-		animated_sprite.play("jump")
-	
 	# Apply movement
 	if direction:
 		velocity.x = direction * SPEED
@@ -59,8 +55,20 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+	# Play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		if velocity.y < 0:
+			animated_sprite.play("jump_up")
+		else:
+			animated_sprite.play("jump_down")
 	
 func _on_animated_sprite_2d_animation_finished():
-	if animated_sprite.animation == "attack" or animated_sprite.animation == "attack2":
+	if animated_sprite.animation in ["attack", "attack2"]:
 		is_attacking = false
 		animated_sprite.play("idle")
